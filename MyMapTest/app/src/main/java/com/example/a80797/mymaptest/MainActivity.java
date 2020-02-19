98a2bcd6c5b75e3320054254f2101abec9bba7b5
Growl <807971999@qq.com>
2020-02-19 16:41:14 +0800

    first commit

---
+++ MyMapTest/app/src/main/java/com/example/a80797/mymaptest/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.a80797.mymaptest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.MyLocationStyle;
+
+
+public class MainActivity extends AppCompatActivity {
+    private MapView mMapView;
+    private AMap aMap;
+    MyLocationStyle myLocationStyle;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMapView = findViewById(R.id.map_view);
+        mMapView.onCreate(savedInstanceState);
+
+        if(aMap==null)
+            aMap=mMapView.getMap();
+        myLocationStyle=new MyLocationStyle();//初始化定位蓝点样式
+        myLocationStyle.interval(2000);//设置连续的定位模式的定位间隔
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);
+//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）默认执行此种模式。
+        myLocationStyle.showMyLocation(true);//显示蓝点
+        aMap.setMyLocationStyle(myLocationStyle);
+        aMap.setMyLocationEnabled(true);//设置true表示启动显示定位蓝点
+
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
+        mMapView.onDestroy();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
+        mMapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
+        mMapView.onPause();
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //在activity执行onSaveInstanceState时执行mMapView.onSaveInstanceState (outState)，保存地图当前的状态
+        mMapView.onSaveInstanceState(outState);
+    }
+
+}
